f1ce107c425090a92f2b2a353eda52bba0cfdd1a
Scoobylolo <andy2002.amc@gmail.com>
2023-03-18 19:41:43 -0400

    Now outputs to a file

--- read_csv.py
+++ read_csv.py
@@ -9,10 +9,12 @@ def get_time(timestamp):
     dt_string = dt_object.strftime("%Y-%m-%d %H:%M:%S %a")
     return dt_string
 
-def read_file(filename):
+def read_file(filename,outfile):
+    as_list=[]
     with open(filename, 'r') as file:
         reader = csv.reader(file)
         passed=False
+        
         for row in reader:
             if len(row) == 6:
                 col1, col2, col3, col4, col5, col6 = row
@@ -22,14 +24,23 @@ def read_file(filename):
                 if not passed:
                     continue
 
-                city = cell_api.get_info(int(col2), int(col3), int(col4), int(col5))
+                what,where,country = cell_api.get_info(int(col2), int(col3), int(col4), int(col5),"city")
 
-                print("Time: {}\n{}: {}\n\n".format(get_time(int(col1)),city))
+                useful_info="Time: {}\n{}: {}, {}\n\n".format(get_time(int(col1)),what,where,country)
+                as_list.append(useful_info)
 
-                # print(col1, col2, col3, col4, col5, col6)
+                print(useful_info)
             else:
                 print('Invalid row:', row)
 
+    with open(outfile, 'w') as f:
+        for item in as_list:
+            f.write("%s" % item)
+        f.close()
+
 if __name__ == '__main__':
-    read_file('CellTowerData.csv')
+    out="locations.log"
+    read_file('CellTowerData.csv',out)
+
+    print("Check the file",out,"for the results.")
 
